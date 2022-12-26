3b045bd517e71898a0b525aeec5f29a3512ad06d
Sylphy0052 <sylphy0052@gmail.com>
2022-12-27 01:42:22 +0900

    [add] template

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-import streamlit as st
+from instance.answer import Answer
+from instance.user import User
+
+if __name__ == "__main__":
+    answer = Answer()
+    user = User()
+    while True:
+        pass
